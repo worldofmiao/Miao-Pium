4b1c68ae2bcf8a51691e426ef1fbe6faa12aac0a
mxiang001c <Miao_Xiang@cable.comcast.com>
2017-07-21 19:39:16 -0700

    implement a simple Android test into project with some feature update in the base MobilePage class.

--- src/main/java/utils/MobilePage.java
+++ src/main/java/utils/MobilePage.java
@@ -1,10 +1,11 @@
 package utils;
 
-import io.appium.java_client.ios.IOSDriver;
+
 import ios.Driver;
 
 import java.util.HashMap;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
@@ -22,5 +23,31 @@ public class MobilePage {
 		HashMap<String, String> scrollObject = new HashMap<String, String>();
 		scrollObject.put("direction", "down");
 		js.executeScript("mobile: scroll", scrollObject);
+	}
+	
+	public boolean elementExist (android.Driver driver, By x )
+	{
+	try
+	{
+	driver.driver.findElement( x );
+	return true;
+	}
+	catch(org.openqa.selenium.NoSuchElementException ex)
+	{
+	return false;
+	}
+	}
+	
+	public boolean elementExist (Driver driver, By x )
+	{
+	try
+	{
+	driver.driver.findElement( x );
+	return true;
+	}
+	catch(org.openqa.selenium.NoSuchElementException ex)
+	{
+	return false;
+	}
 	}
 }
